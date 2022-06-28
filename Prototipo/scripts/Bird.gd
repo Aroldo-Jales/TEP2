b98f316c1dc502bfe69f03785ce4cae8a941b8ef
Ch4seR S <aroldo_jales@hotmail.com>
2022-06-28 09:21:15 -0300

    feature: game menu and score

--- Prototipo/scripts/Bird.gd
+++ Prototipo/scripts/Bird.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
 export (int) var gravity = 0
-
 var velocity
+
+signal bird_fall
+var bird_is_fall: bool = false
 
 func _init():
 	velocity = Vector2.ZERO
@@ -11,7 +13,7 @@ func _init():
 func _physics_process(delta):	
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)	
-		
+			
 	var player = get_tree().get_root().get_node("Main").get_node("Player")			
 	var player_pos_x = player.position.x
 	var player_pos_y = player.position.y
@@ -19,7 +21,12 @@ func _physics_process(delta):
 	var bird_pos_x = position.x
 	var bird_pos_y = position.y
 		
-	if(bird_pos_x <= player_pos_x and bird_pos_y >= player_pos_y):
+	if(bird_pos_x <= player_pos_x and bird_pos_y >= player_pos_y):			
+		if(!bird_is_fall):
+			bird_is_fall = true
+			$LabelBirdScore.visible = true					
+			emit_signal('bird_fall')		
+			
 		velocity.x = -900
 		gravity = 5000		
 
@@ -27,3 +34,6 @@ func _physics_process(delta):
 	if(position.x < -250 or position.y > 900):
 		print(">>>>>> bird_removed >>>>>>")
 		queue_free()	
+
+func _on_BirdArea_area_entered(area):
+	get_tree().change_scene("res://scenes/ui/Menu.tscn")
--- Prototipo/scripts/Main.gd
+++ Prototipo/scripts/Main.gd
@@ -1,26 +1,32 @@
 extends Node2D
 
+var score: int = 0
 var enemy_time = 0.5
 var enemy_velocity = Vector2(-900, 0)
 
-func _ready() -> void:
+var water_active: bool = false
+
+func _ready():
 	# WALLS
 	_add_wall(Vector2(0, -10), Vector2(1600, 10)) # TOP
-	_add_wall(Vector2(0, 900), Vector2(1600, 50)) # BOTTOM	
+	_add_wall(Vector2(0, 750), Vector2(1600, 50)) # BOTTOM	
 	_add_wall(Vector2(-10, -10), Vector2(10, 1600)) # LEFT
 	_add_wall(Vector2(1610, -10), Vector2(10, 1600)) # RIGHT	
+		
+	#$ButtonPause.connect('pause_game', self, '_on_Pause_game')
 	
 func _on_Timer_timeout():
-	
+	#TIMER ENEMIES
 	randomize()
-	var bird = preload("res://scenes/Bird.tscn").instance()
+	var bird = preload("res://scenes/game/Bird.tscn").instance()
 	var rand_y = rand_range(0, 650)
 	bird.global_position = Vector2(1620, rand_y)			
-	add_child(bird)		
+	add_child(bird)			
+	bird.connect('bird_fall', self, '_on_Bird_fall')
 	
 	$TimerEnemies.wait_time = enemy_time
 	
-func _add_wall(position: Vector2, size: Vector2) -> void:
+func _add_wall(position: Vector2, size: Vector2):
 	var rect := RectangleShape2D.new()
 	rect.set_extents(size)
 	
@@ -37,3 +43,15 @@ func _add_wall(position: Vector2, size: Vector2) -> void:
 		
 	add_child(collision_object)
 	
+func _on_TimerScore_timeout():
+	#TIMER SCORE	
+	$LabelScore.text = str(score)	
+	score += int(1)
+	
+	if(!water_active):
+		water_active = true
+		$WaterBottom.visible = true
+
+func _on_Bird_fall():
+	$LabelScore.text = str(score)
+	score += int(5)
